4e6b4311462285cf6698827dc05c987840ea2bb0
Abhijith44121 <abhijithabhi508108@gmail.com>
2026-02-06 19:58:22 +0530

    Add full React shopping cart UI

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,23 @@
+import ProductCard from "./ProductCard";
+import { useProducts } from "../context/ProductContext";
+const ProductList = () => {
+
+    const { products,error,loading } = useProducts();
+
+    return (
+        
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+
+
+                         {loading && <p>Loading...</p>}
+                        {error && <div className='error'>X {error}</div>}
+
+                        { products.map((product) => (
+                            <ProductCard product={product} key ={product.id} /> 
+                        
+                        )) }
+                </div>
+            
+     )};
+ 
+export default ProductList;
